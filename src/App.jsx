0c20b17a1d441d8c2951f3b6f6186896d8600c0b
sayanbose-0000 <bosesayan0000@gmail.com>
2025-03-13 23:07:59 +0530

    basic file open and monaco shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,40 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import { invoke } from "@tauri-apps/api/tauri";
-// import "./App.css";
-
-// function App() {
-//   const [greetMsg, setGreetMsg] = useState("");
-//   const [name, setName] = useState("");
-
-//   async function greet() {
-//     // Learn more about Tauri commands at https://v1.tauri.app/v1/guides/features/command
-//     setGreetMsg(await invoke("greet", { name }));
-//   }
-
-//   return (
-//     <main className="container">
-//       <h1>Welcome to Tauri + React</h1>
-
-//       <div className="row">
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-//         </a>
-//         <a href="https://tauri.app" target="_blank">
-//           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-//       <form
-//         className="row"
-//         onSubmit={(e) => {
-//           e.preventDefault();
-//           greet();
-//         }}
-//       >
-//         <input
-//           id="greet-input"
-//           onChange={(e) => setName(e.currentTarget.value)}
-//           placeholder="Enter a name..."
-//         />
-//         <button type="submit">Greet</button>
-//       </form>
-//       <p>{greetMsg}</p>
-//     </main>
-//   );
-// }
-
-// export default App;
+import { Editor } from "@monaco-editor/react";
+import { open } from "@tauri-apps/api/dialog";
+import { readTextFile } from "@tauri-apps/api/fs";
+import { useState } from "react";
 
 
+const App = () => {
+  const [fileContent, setFileContent] = useState("");
+  const [extension, setExtension] = useState("");
 
+  const handleFileRead = async () => {
+    const fileSelect = await open();
+    const arr = fileSelect.split("\\");
 
+    const lastIdx = arr[arr.length - 1];
 
-import { invoke } from "@tauri-apps/api/tauri"
-import React, { useState } from "react";
+    const extWord = lastIdx.split(".");
+    const ext = extWord[extWord.length - 1];
 
-function App(){
-  const [folderPath,setFolderPath] = useState("");
+    setExtension(ext);
 
-  const openFolder = async()=>{
-    const path = await invoke("open_folder");
-    if(path){
-      console.log("Selected Folder:",path);
-      setFolderPath(path);
+    if (fileSelect) {
+      const content = await readTextFile(fileSelect);
+      setFileContent(content);
     }
-  };
-  return(
+
+    else {
+      console.log("No files selected");
+    }
+  }
+
+  return (
     <div>
-      <button onClick={openFolder}>Open Folder</button>
-      {folderPath && <p>Selected Folder: {folderPath}</p>}
+      <button onClick={handleFileRead}>Open File</button>
+      <Editor height="90vh" language={extension} value={fileContent} />;
     </div>
   );
 }
+
+export default App;
